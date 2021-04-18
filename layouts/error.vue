52c0d4a505782ac9f10b38c019a627f9aaf6fd7d
Micah Walter <micah@micahwalter.com>
2021-04-18 14:54:16 -0400

    consistent styles

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
+  <article>
     <h1 v-if="error.statusCode === 404">404. Page not found</h1>
     <h1 v-else>An error occurred</h1>
     <NuxtLink to="/">Home page</NuxtLink>
-  </div>
+  </article>
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,13 @@
 <template>
-  <div>
-      <h1>
-        Hi there 👋, I'm Micah Walter
-      </h1>
-      <p class="subtitle">Welcome to my website.</p>
-      <p>More soon!</p>
-      <hr>
+  <article>
+    <h1>Hi there 👋, I'm Micah Walter</h1>
+    <p class="subtitle">Welcome to my website.</p>
+    <p>More soon!</p>
+    <hr>
+    <section>
       <p>You can usually find me <a href="https://twitter.com/micahwalter">here</a>.</p>
       <p>I also sometimes write a little newsletter <a href="https://world.hey.com/micah">here</a>.</p>
-
-
-  </div>
+    </section>
+  </article>
 </template>
 
