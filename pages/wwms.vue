5772140a6ca5e68dc47b2fd156d747ac0d7e3f26
Micah Walter <micah@micahwalter.com>
2021-04-22 16:00:18 -0400

    wwms, the webpage

---
+++ pages/wwms.vue
@@ -0,0 +1,26 @@
+<template>
+  <p v-if="$fetchState.pending">Fetching mountains...</p>
+  <p v-else-if="$fetchState.error">An error occurred :(</p>
+  <div v-else>
+    <h1>What Would Micah Say?</h1>
+
+    <blockquote>{{ micah.micah.says }}</blockquote>
+
+    <button @click="$fetch">Refresh</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        micah: {}
+      }
+    },
+    async fetch() {
+      this.micah = await fetch(
+        'https://api.micahwalter.com/wwms'
+      ).then(res => res.json())
+    }
+  }
+</script>
