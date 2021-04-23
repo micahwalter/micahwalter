f4875f771f31dc5d362f8c28697e1d690b411841
Micah Walter <micah@hey.com>
2021-04-22 22:54:17 -0400

    styles

--- pages/wwms.vue
+++ pages/wwms.vue
@@ -1,13 +1,16 @@
 <template>
-  <p v-if="$fetchState.pending">Fetching saying...</p>
-  <p v-else-if="$fetchState.error">An error occurred :(</p>
-  <div v-else>
+  <article>
     <h1>What Would Micah Say?</h1>
-
-    <blockquote>{{ micah.micah.says }}</blockquote>
-
-    <button @click="$fetch">Refresh</button>
-  </div>
+    <hr>
+      <section>
+        <p v-if="$fetchState.pending">Fetching saying...</p>
+        <p v-else-if="$fetchState.error">An error occurred :(</p>
+        <div v-else>
+          <blockquote>{{ micah.micah.says }}</blockquote>
+          <button @click="$fetch">Refresh</button>
+        </div>
+      </section>
+  </article>
 </template>
 
 <script>
