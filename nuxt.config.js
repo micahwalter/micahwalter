5671d69a13a1d39e10618f1735a9d9421dda68fe
Micah Walter <micah@hey.com>
2021-06-01 13:26:36 +0000

    trying ssr true

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ const meta = getSiteMeta();
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
