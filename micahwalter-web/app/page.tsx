ebfd882b995533c527ab9ae07aaa1d8e2272a307
Micah Walter <47419+micahwalter@users.noreply.github.com>
2024-01-10 20:37:15 +0000

    adding an image with lfs

--- micahwalter-web/app/page.tsx
+++ micahwalter-web/app/page.tsx
@@ -1,9 +1,16 @@
 import Image from 'next/image'
+import christmasPic from '../public/images/IMG_4128.jpg'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl">Hello</h1>
+
+      <Image
+        src={christmasPic}
+        alt="Christmas at the Walter home."
+      />
+      
     </main>
   )
 }
