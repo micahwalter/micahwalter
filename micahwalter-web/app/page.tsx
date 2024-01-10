7ae3adaec620d5387bcbecc39227e3831b906cc1
Micah Walter <47419+micahwalter@users.noreply.github.com>
2024-01-10 19:51:07 +0000

    font stuff

--- micahwalter-web/app/page.tsx
+++ micahwalter-web/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Hello</p>
+      <h1 className="text-4xl">Hello</h1>
     </main>
   )
 }
